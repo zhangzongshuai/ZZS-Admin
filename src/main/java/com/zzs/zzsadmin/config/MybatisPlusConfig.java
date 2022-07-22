3f9374b7c3b4ba41acb6158d978a189d9e5346d9
张宗帅 <t-zongshuai.zhang@pcitc.com>
2022-07-22 21:36:52 +0800

    zzs

---
+++ src/main/java/com/zzs/zzsadmin/config/MybatisPlusConfig.java
@@ -0,0 +1,25 @@
+package com.zzs.zzsadmin.config;
+
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * <p>mybatis-plus分页插件</p>
+ *
+ * @author 张宗帅
+ * @date 2022-07-22 15:51:08
+ */
+@Configuration
+public class MybatisPlusConfig {
+
+    @Bean
+    public MybatisPlusInterceptor mybatisPlusInterceptor() {
+        MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+        interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL));
+        return interceptor;
+    }
+
+}
